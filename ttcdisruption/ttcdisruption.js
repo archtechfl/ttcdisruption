b9dd0830e57edc2ea10d4da5d27422b4c2472041
Jeremy Ubuntu <archtechfl@gmail.com>
2015-08-11 19:42:00 -0400

    connected to twitter

--- ttcdisruption/ttcdisruption.js
+++ ttcdisruption/ttcdisruption.js
@@ -12,5 +12,26 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+
+    var Twit = Meteor.npmRequire('twit');
+
+    var T = new Twit({
+        consumer_key:         Meteor.settings.consumer_key, // API key
+        consumer_secret:      Meteor.settings.consumer_secret, // API secret
+        access_token:         Meteor.settings.access_token, 
+        access_token_secret:  Meteor.settings.access_token_secret
+    });
+
+    //  search twitter for all tweets containing the word 'banana'
+    //  since Nov. 11, 2011
+    T.get('statuses/user_timeline',
+        {
+            screen_name: 'TTCnotices'
+        },
+        function(err, data, response) {
+            console.log(data[0]);
+        }
+    );
+
   });
 }
