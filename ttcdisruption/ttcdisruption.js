c6477f077d8d21cdeb9f2a12fa6b5822b9ed3b1e
Jeremy Ubuntu <archtechfl@gmail.com>
2015-08-11 18:43:54 -0400

    stripped out intro work

--- ttcdisruption/ttcdisruption.js
+++ ttcdisruption/ttcdisruption.js
@@ -1,18 +1,11 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
+  Template.ttcdisruption.helpers({
+    // Helpers go here
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
+  Template.ttcdisruption.events({
+    // Events go here
   });
 }
 
