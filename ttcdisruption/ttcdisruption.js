2520d3ae76193bf229638acae912ddafcb209d8c
Jeremy Ubuntu <archtechfl@gmail.com>
2015-08-12 23:34:51 -0400

    added font awesome

--- ttcdisruption/ttcdisruption.js
+++ ttcdisruption/ttcdisruption.js
@@ -13,16 +13,52 @@ if (Meteor.isClient) {
   Template.body.events({
     // Events go here
   });
+
+  // Determine if owner
+  Template.ttcdisruption.helpers({
+    // Owner is defined when the task is created, set to the user ID that created it
+    isSubway: function () {
+        // Get the text
+        var text = this.description;
+        // Check for mention of line and a number, ex. "line 1"
+        var subwayCheck = text.search("trains");
+        if (subwayCheck != -1){
+          return true;
+        } else {
+          return false;
+        }
+    }
+  });
+
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
-
     Notices.remove({});
+    // begin tweet retrieval cycle for user TTCalerts
+    /* 
+    Goal: retrieve all 3200 accessible tweets by cycling through 200 tweet
+    chunks 16 times
 
+    Each time the cycle ends, get the tweet ID of the oldest tweet and set the
+    next "max_id" parameter to that ID to get the next 200 oldest tweets
+    */
+    Meteor.call("getTweets");
+  });
+}
+
+// End of server code
+
+Meteor.methods({
+  test: function (oldestTweet) {
+    // This is a generic testing function confirming that methods are hooked up
+    console.log(oldestTweet);
+  },
+  getTweets: function () {
     var Twit = Meteor.npmRequire('twit');
 
+    // Create new twitter access object
     var T = new Twit({
         consumer_key:         Meteor.settings.consumer_key, // API key
         consumer_secret:      Meteor.settings.consumer_secret, // API secret
@@ -30,31 +66,45 @@ if (Meteor.isServer) {
         access_token_secret:  Meteor.settings.access_token_secret
     });
 
-    //  search twitter use timeline "TTCalerts"
-    T.get('statuses/user_timeline',
-        {
-            screen_name: 'TTCnotices',
-            exclude_replies: true,
-            count: 200
-        },
-        // callback for Twitter API query has to be bound to Meteor as follows:
-        Meteor.bindEnvironment(function(err, data, response) {
-            _.each(data, function (item){
-              // Go through the data and add it to the Notices collection
-              // First, turn to lowercase
-              var itemText = item.text;
-              var itemLowerCase = itemText.toString().toLowerCase();
-              // Find tweets with all clear
-              var allClear = itemLowerCase.search("all clear");
-              // Don't add "all clear" tweets to database
-              if (allClear == -1){
-                Notices.insert({
-                  description: itemLowerCase
-                });
-              }
-            });
-        })
-    );
+    // Parameters object
+    var tweetParameters = {
+        screen_name: 'TTCnotices',
+        count: 200
+    };
 
-  });
-}
+    var totalRetrieved = 0;
+
+    for (var counter = 0; counter < 1; counter++){
+        // Search twitter user timeline "TTCalerts"
+        T.get('statuses/user_timeline',
+            tweetParameters,
+            // callback for Twitter API query has to be bound to Meteor as follows:
+            Meteor.bindEnvironment(function(err, data, response) {
+                _.each(data, function (item, index){
+                  // Go through the data and add it to the Notices collection
+                  // First, turn to lowercase
+                  var itemText = item.text;
+                  var itemLowerCase = itemText.toString().toLowerCase();
+                  // Find tweets with all clear
+                  var allClear = itemLowerCase.search("all clear");
+                  // Find tweets with rt @user_name (retweets)
+                  var retweet = itemLowerCase.search("@");
+                  // Don't add "all clear" tweets to database
+                  if (allClear == -1 && retweet == -1){
+                    Notices.insert({
+                      description: itemLowerCase,
+                      time: item.created_at,
+                      tweet_id: item.id
+                    });
+                  }
+                  if (index === 199){
+                    var oldestTweet = item.id;
+                    totalRetrieved = totalRetrieved + (index + 1);
+                    tweetParameters["max_id"] = oldestTweet;
+                  }
+                });
+            })
+        );
+    }// End loop
+  }// End get tweets method
+});//End meteor methods
