3088023d71d187e7d2045a8c5b146c79accf3cdc
Jeremy Ubuntu <archtechfl@gmail.com>
2015-08-14 14:36:31 -0400

    server code now in server folder

--- ttcdisruption/ttcdisruption.js
+++ ttcdisruption/ttcdisruption.js
@@ -300,24 +300,6 @@ if (Meteor.isClient) {
 
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-    Notices.remove({});
-    // begin tweet retrieval cycle for user TTCalerts
-    /* 
-    Goal: retrieve all 3200 accessible tweets by cycling through 200 tweet
-    chunks 16 times
-
-    Each time the cycle ends, get the tweet ID of the oldest tweet and set the
-    next "max_id" parameter to that ID to get the next 200 oldest tweets
-    */
-    Meteor.call("getTweets");
-  });
-}
-
-// End of server code
-
 Meteor.methods({
   test: function (oldestTweet) {
     // This is a generic testing function confirming that methods are hooked up
